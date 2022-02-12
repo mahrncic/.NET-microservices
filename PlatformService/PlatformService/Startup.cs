ee6fb1a580e1e8c10268d54380e44c3d7b42a5c2
Marko Hrncic <marko.hrncic@kogitas.com>
2022-02-12 19:52:33 +0100

    Registered PlatformRepo dependency injection in Startup class.

--- PlatformService/PlatformService/Startup.cs
+++ PlatformService/PlatformService/Startup.cs
@@ -30,6 +30,8 @@ namespace PlatformService
             services.AddDbContext<AppDbContext>(opt => 
                 opt.UseInMemoryDatabase("InMem"));
 
+            services.AddScoped<IPlatformRepo, PlatformRepo>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
