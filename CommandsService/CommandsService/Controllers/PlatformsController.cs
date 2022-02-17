43fa1703e397c7b5aed221dcff7627dc94a12805
Marko Hrncic <marko.hrncic@kogitas.com>
2022-02-17 19:23:16 +0100

    Added TestInboundConnection endpoint to Platforms controller.

---
+++ CommandsService/CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandsService.Controllers
+{
+    [ApiController]
+    [Route("api/c/[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Commands Service");
+
+            return Ok("Inbound test OK from Platforms Controller");
+        }
+    }
+}
