5fdbab6a60935ea2efc957c9b83939e840dc5992
Marko Hrncic <marko.hrncic@kogitas.com>
2022-02-17 20:24:15 +0100

    Added functionality inside HttpCommandDataClient to send platform to command service.

---
+++ PlatformService/PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using PlatformService.Dtos;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient client, IConfiguration configuration)
+        {
+            _httpClient = client;
+            _configuration = configuration;
+        }
+
+        public async Task SendPlatformToCommand(PlatformReadDto platform)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(platform),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await _httpClient.PostAsync(_configuration["CommandServiceUrl"], httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> Sync POST to Commands Service was OK!");
+            }
+            else
+            {
+                Console.WriteLine("--> Sync POST to Commands Service was NOT OK!");
+            }
+        }
+    }
+}
