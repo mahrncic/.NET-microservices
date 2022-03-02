bc3efe13e80ced8782345624ea59adeed0188f9f
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-02 19:57:39 +0100

    Added Command and Platform Models.

---
+++ CommandsService/CommandsService/Models/Platform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CommandsService.Models
+{
+    public class Platform
+    {
+        public int Id { get; set; }
+        public int ExternalId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Command> Commands { get; set; } = new List<Command>();
+    }
+}
