41b8ef03c5ea0baead6054c9ff053b4680ff0cbc
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-02 19:59:19 +0100

    Added annotations to Command and Platform models.

--- CommandsService/CommandsService/Models/Command.cs
+++ CommandsService/CommandsService/Models/Command.cs
@@ -1,10 +1,16 @@
-﻿namespace CommandsService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Models
 {
     public class Command
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public int PlatformId { get; set; }
+        [Required]
         public string HowTo { get; set; }
+        [Required]
         public string CommandLine { get; set; }
         public Platform Platform { get; set; }
     }
