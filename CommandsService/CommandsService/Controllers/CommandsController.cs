7183d4f4f7a228b8b9af13deb8476d8d4dc9a581
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-02 21:49:07 +0100

    Implemented Get Command For Platform endpoint.

---
+++ CommandsService/CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace CommandsService.Controllers
+{
+    [ApiController]
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    public class CommandsController: ControllerBase
+    {
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommandRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsByPlatform(int platformId)
+        {
+            Console.WriteLine($"--> Getting Commands by Platform: {platformId}");
+
+            if (!_repository.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+
+            var commands = _repository.GetCommandsForPlatform(platformId);
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+
+        [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+        public ActionResult<CommandReadDto> GetCommandForPlatform(int platformId, int commandId)
+        {
+            Console.WriteLine($"--> Getting Command: {commandId} for Platform: {platformId}");
+
+            if (!_repository.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+
+            var command = _repository.GetCommand(platformId, commandId);
+
+            return Ok(_mapper.Map<CommandReadDto>(command));
+        }
+    }
+}
