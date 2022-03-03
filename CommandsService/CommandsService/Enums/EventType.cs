40aceaeb004e76c7ff5ea0e081d3cdc4acd8ec0c
Marko Hrncic <marko.hrncic@kogitas.com>
2022-03-03 19:59:55 +0100

    Implemented changes in Event Processor for determining event type.

---
+++ CommandsService/CommandsService/Enums/EventType.cs
@@ -0,0 +1,8 @@
+﻿namespace CommandsService.Enums
+{
+    public enum EventType
+    {
+        PlatformPublished,
+        Undetermined
+    }
+}
---
+++ CommandsService/CommandsService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using CommandsService.Dtos;
+using CommandsService.Enums;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Text.Json;
+
+namespace CommandsService.EventProcessing
+{
+    public class EventProcessor : IEventProcessor
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(IServiceScopeFactory scopeFactory, IMapper mapper)
+        {
+            _scopeFactory = scopeFactory;
+            _mapper = mapper;
+        }
+
+        public void ProcessEvent(string message)
+        {
+            
+        }
+
+        private EventType DetermineEventType(string notificationMessage)
+        {
+            Console.WriteLine("--> Determining Event Type...");
+
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+
+            switch (eventType.Event) 
+            {
+                case "Platform_Published":
+                    Console.WriteLine("--> Platform Published Event Detected!");
+                    return EventType.PlatformPublished;
+                default:
+                    Console.WriteLine("--> Event Type Undetermined!");
+                    return EventType.Undetermined;
+            }
+        }
+    }
+}
